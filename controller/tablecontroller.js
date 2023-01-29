8cc479d2b9ac060ee32273c845b8cbd6b0239fd4
alroyserrao <68016670+alroyserrao@users.noreply.github.com>
2023-01-29 18:58:06 +0530

    Alroy>Table Get Function(Redux)

---
+++ controller/tablecontroller.js
@@ -0,0 +1,13 @@
+import studentDataSchema from '../model/studentSchema.js'
+import studentRequest from '../model/studentLoginSchema.js';
+
+export const getTables = async (request, response) => {
+    try {
+        const tables = await studentRequest.find({});
+
+        response.json(tables);
+    }catch (error) {
+        console.error(error.message);
+
+    }
+}
