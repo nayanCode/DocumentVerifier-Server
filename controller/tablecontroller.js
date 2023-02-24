e2ec225e519e60e4b752acc8ad9610a8369be8fb
nayan94265 <nayanshingare93@gmail.com>
2023-02-24 20:44:04 +0530

    nayan ==> status Backend

--- controller/tablecontroller.js
+++ controller/tablecontroller.js
@@ -4,10 +4,10 @@ import studentRequest from '../model/studentLoginSchema.js';
 export const getTables = async (request, response) => {
     try {
         const tables = await studentRequest.find({});
-
         response.json(tables);
     }catch (error) {
         console.error(error.message);
-
     }
 }
+
+///
