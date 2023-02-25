2b10658654983ffdc09dc536b0ca0ebd376be5e3
alroyserrao <68016670+alroyserrao@users.noreply.github.com>
2023-02-25 18:08:02 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,17 @@
 import express from  'express';
 // import StudentLogin from '../../client/src/Components/StudentLogin.js';
 import studentRequest from '../model/studentLoginSchema.js';
-import {studentRequestcontroller, userLogIn} from '../controller/studentcontroller.js';
+import {studentRequestcontroller, userLogIn,existStudent,getStudentStatus,updateStatus,updateHash} from '../controller/studentcontroller.js';
 import {getTables} from '../controller/tablecontroller.js'
 
 const router = express.Router();
 
 router.post('/login', userLogIn);
 router.post('/sentRequest', studentRequestcontroller);
+router.post('/verifyExistence', existStudent);
+router.get('/getStatus/:id', getStudentStatus);
+router.post('/updateStatus/:id/request=:newMessage',updateStatus);
+router.post('/updateHash/:id/request=:hash',updateHash);
 router.get('/tables', getTables);
 
 // router.route("/table").get((req,res) => {
